6b2f93184b2e6b329d6e5c3c9ae1778d49f7db7c
Leonardo Rocca <lrocca@student.42roma.it>
2022-01-17 17:05:56 +0100

    ex00: should be fine

---
+++ ex00/convert.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <limits>
+
+void	printType(const std::string& type)
+{
+	std::cout << type << ": ";
+}
+
+void	cChar(double val)
+{
+	char	c = val;
+
+	if (val != val || !isascii(val))
+		std::cout << "impossible";
+	else if (isprint(c))
+		std::cout << "'" << c << "'";
+	else
+		std::cout << "Non displayable";
+}
+
+void	cInt(double val)
+{
+	int	i = val;
+
+	if (val != val || val < INT_MIN || val > INT_MAX)
+		std::cout << "impossible";
+	else
+		std::cout << i;
+}
+
+void	cFloat(double val)
+{
+	float	f = val;
+
+	std::cout << f << "f";
+}
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+		return 1;
+
+	std::string	num = av[1];
+
+	double	val = atof(av[1]);
+
+	std::cout.precision(1);
+	std::cout << std::fixed;
+
+	printType("char");
+	cChar(val);
+	std::cout << std::endl;
+
+	printType("int");
+	cInt(val);
+	std::cout << std::endl;
+
+	printType("float");
+	cFloat(val);
+	std::cout << std::endl;
+
+	printType("double");
+	std::cout << val << std::endl;
+}
